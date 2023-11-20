a24465f337a89758065cc23814e121d0bcd5bb5f
bcarrans <bcarranc@ucm.es>
2023-11-21 00:23:13 +0100

    .

--- run_script.php
+++ run_script.php
@@ -3,11 +3,16 @@ chdir(__DIR__);
 
 //$maude_module = str_replace('\n', "\n", $_POST["maude_module"]);
 
+
 $maude_module = $_POST["maude_module"];
 $maude_command = $_POST["maude_command"];
 
+$maude_module = str_replace(array("\r", "\n"), ' ', $maude_module);
+$maude_command = str_replace(array("\r", "\n"), ' ', $maude_command);
+
 $escaped_maude_module = escapeshellcmd($maude_module);
 $escaped_maude_command = escapeshellcmd($maude_command);
+
 file_put_contents("temp_module.txt", $maude_module);
 file_put_contents("temp_command.txt", $maude_command);
 
--- script.py
+++ script.py
@@ -3,18 +3,39 @@
 import sys
 import maude
 
+def getCommand(c):
+    c2 = c.split()
+    command = c2[0]
+    term = ' '.join(c2[1:])[:-1]
+    return command, term
+
+
 maude.init()
 
-maude_module = sys.argv[1]
-maude_command = sys.argv[2]
+maude_module = sys.argv[1].replace("^", "")
+maude_command = sys.argv[2].replace("^", "")
 
 print("Module: " + maude_module)
-print(" Command: " + maude_command)
 
-m = maude.getModule(maude_module)
+#m = maude.getModule(maude_module)
+#m = maude.downModule()
+maude.input(maude_module)
+m = maude.getCurrentModule()
 
-t = m.parseTerm(maude_command)
-t.reduce()
+command, term = getCommand(maude_command)
+
+print("Command: " + command)
+print("Term: " + term)
+
+t = m.parseTerm(term)
+
+
+if command == "red" or command == "reduce":
+    t.reduce()
+elif command == "rew":
+    t.rewrite()
+else:
+    t = "No se ha reconocido el comando introducido"
 
 print("<html><body>")
 print("<h4>Result:</h4>")
